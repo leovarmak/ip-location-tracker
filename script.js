e4e0d1cc0f715985c98e1522dba82cd1f84eb1ab
Karthik Varma <leovarmak@hotmail.com>
2018-08-13 21:47:19 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const axios = require('axios');
+const publicip = require('public-ip');
+
+const url = 'https://www.maxmind.com/geoip/v2.1/city/'
+publicip.v4().then(ip => {
+    axios.get(url + ip + '?demo=1').then(function(res){
+    	let data = res.data;
+    	console.log('IP: ' + ip)
+    	console.log('City: ' + data.city.names.en + ', ' + data.subdivisions[0].names.en)
+    	console.log('Country: ' + data.country.names.en)
+    })
+});
